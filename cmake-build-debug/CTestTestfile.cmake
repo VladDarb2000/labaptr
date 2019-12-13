5e90d96cef593d81888701e28829e91b7d1e4333
VladDarb2000 <Vlad.darbinyan.00@mail.ru>
2019-12-13 21:55:43 +0300

    Lo

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/threelab/threeptr
+# Build directory: /cygdrive/c/threelab/threeptr/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/cygdrive/c/threelab/threeptr/cmake-build-debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/c/threelab/threeptr/CMakeLists.txt;66;add_test;/cygdrive/c/threelab/threeptr/CMakeLists.txt;0;")
